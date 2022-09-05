de61df1f82402e03e4994a8c969add8a36248ef8
berkay <berkayert7@gmail.com>
2022-09-05 19:14:54 +0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Berkay Ertürk on 5.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/abcApp.swift
@@ -0,0 +1,17 @@
+//
+//  abcApp.swift
+//  Shared
+//
+//  Created by Berkay Ertürk on 5.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct abcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
